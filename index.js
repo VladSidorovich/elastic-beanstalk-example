d5d209d7d9a51b85160dd12a0b522fdf9b662bec
vladsidorovi4@gmail.com <vladsidorovi4@gmail.com>
2022-01-02 23:46:12 +0300

    fix

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.port || 3000;
 
 app.use(express.static("public"));
 
+app.get("/test", (_, res) => res.send("test"));
+
 app.listen(port, () => {
   console.log(`CV app listening at http://localhost:${port}`);
 });
